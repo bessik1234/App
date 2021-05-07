d5b3af9dc86a0aa6ef927a651fe6eec3cb0453da
Bessik <bessik1234@bk.ru>
2021-05-07 15:58:58 +0300

    rename app.module.css

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import InputItem from './components/InputItem/InputItem';
 import ItemList from './components/ItemList/ItemList';
 import Footer from './components/Footer/Footer';
-import './App.css';
+import styles from './App.module.css';
 
 const App = () => {
  
@@ -20,8 +20,8 @@ const App = () => {
 
 ];
   return(
-  <div className="wrap">
-  <h1 className="wrap__title">Важные дела:</h1>
+  <div className={styles.wrap}>
+  <h1 className={styles.title}> Важные дела: </h1>
   <InputItem items={items} />
   <ItemList doneItem={1}/>
   <Footer count={3} />
